33358b5b30f4ce9a5d8c143799b76159e95f0b1f
TheProGhost <manan05vyas99.com>
2022-08-27 19:51:39 +0530

    Basic Script to install dev tools for py,c,c++

---
+++ devTools_Script.ps1
@@ -0,0 +1,31 @@
+# installing chocolatey as packagemanager
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+Start-Sleep -Seconds 2
+
+#installing gcc,g++
+choco install mingw -y --force
+
+Start-Sleep -Seconds 2
+
+#installing python
+choco install python -y --force
+
+Start-Sleep -Seconds 2
+
+#installing vscode
+choco install vscode -y --force
+
+Start-Sleep -Seconds 2
+
+#installing cpp extensions
+choco install vscode-cpptools -y --force
+
+Start-Sleep -Seconds 2
+
+#installing python extension
+choco install vscode-python -y --force
+
+Write-Output "\n================\nRestarting......\n===============\n"
+
+Restart-Computer
